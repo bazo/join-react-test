37d85a0cad91c4b3582f75a3ad6fbb320bac46c1
Martin Bazik <martin@bazo.sk>
2020-03-30 01:18:05 +0200

    add state and time

--- storage/saveApplication.go
+++ storage/saveApplication.go
@@ -3,12 +3,16 @@ package storage
 import (
 	"context"
 	"join-react-test/types"
+	"time"
 )
 
 func (s *Storage) SaveApplication(application *types.Application) error {
 	ctx := context.Background()
 
 	ref := s.f.Collection(APPLICATIONS).NewDoc()
+
+	application.State = "submitted"
+	application.AppliedOn = time.Now()
 
 	_, err := ref.Set(ctx, application)
 
