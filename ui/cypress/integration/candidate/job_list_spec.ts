2d7fd09ac2fa47374afa31abedf5c9a4b8669c4a
Martin Bazik <martin@bazo.sk>
2020-03-30 23:23:34 +0200

    visit job subpage

--- ui/cypress/integration/candidate/job_list_spec.ts
+++ ui/cypress/integration/candidate/job_list_spec.ts
@@ -10,4 +10,15 @@ context("JobList", () => {
 			expect($p).to.have.length(11);
 		});
 	});
+
+	it("shows job", () => {
+		cy.visit("/");
+		//cy.wait(["@apiRequests"]);
+		const jobLink = cy.get(".job").first();
+		jobLink.invoke("text").then(function(jobTitle) {
+			console.log(jobTitle);
+			cy.visit("/positions/XScInREAACUAFCii");
+			cy.contains(jobTitle);
+		});
+	});
 });
